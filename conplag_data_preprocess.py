b5a09ad88861d8945a004c7dda303ef5fb36b519
sgarnica1 <sgarnica1902@gmail.com>
2025-04-23 17:27:40 -0600

    feat(data): script to preprocess conplag data files

---
+++ conplag_data_preprocess.py
@@ -0,0 +1,26 @@
+import os
+import shutil
+
+files_path = "./research/datasets/conplag"  # Path to the directory containing the files
+destination_path = "./research/datasets/conplag_preprocesed"
+
+os.makedirs(destination_path, exist_ok=True)
+
+files_dict = {}
+
+# Traverse the directory structure and collect unique files
+for dir in os.listdir(files_path):
+    for file in os.listdir(os.path.join(files_path, dir)):
+        if file.endswith(".java") and file not in files_dict:
+            files_dict[file] = os.path.join(files_path, dir, file)
+
+print(f"{len(files_dict)} unique files found.")
+
+files_set = set(files_dict.values())
+
+print("Copying files...")
+# Copy files to the destination directory
+for file in files_set:
+    shutil.copy(file, destination_path)
+
+print(f"{len(files_set)} files copied successfully to '{destination_path}'.")
